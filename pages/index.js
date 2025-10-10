02e42af15ec76d35547e1ba8fe8c504ca4060ba3
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-10 16:19:53 +0330

    Adding Regular React Components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import { getFeaturedEvents } from "../dummy-data";
 export default function HomePage() {
-    return (
-        <div>home page</div>
-    );
+  const featuredEvents = getFeaturedEvents();
+  return (
+    <div>
+      <ul>
+        
+      </ul>
+    </div>
+  );
 }
