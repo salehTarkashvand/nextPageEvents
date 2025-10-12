01f9e1cd4d5962ff7c01b315ff9a22f49da25e5e
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-12 18:48:17 +0330

    Working on the All Events Page

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <Fragment>
       <MainHeader />
-      <main> {children}</main>
+      <main>{children}</main>
     </Fragment>
   );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,13 @@
+import EventList from "../../components/event/event-list";
+import { getAllEvents } from "../../dummy-data";
+
 export default function AllEventPage() {
-    return (
-        <div>all Events</div>
-    );
+  const event = getAllEvents();
+  console.log(event);
+  
+  return (
+    <div>
+      <EventList items={event} />
+    </div>
+  );
 }
