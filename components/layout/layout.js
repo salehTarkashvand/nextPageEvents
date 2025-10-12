59653e4bd1538b18a21824a9deace144784ced3e
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-12 18:28:46 +0330

    adding a General Layout Wrapper Component

---
+++ components/layout/layout.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import MainHeader from "./main-header";
+
+export default function Layout({ children }) {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main> {children}</main>
+    </Fragment>
+  );
+}
